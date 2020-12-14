7f1a220e2c617346710a730de65b141fc7c339a8
ivan-zhukouski <stigmatix.by@gmail.com>
2020-12-14 23:50:51 +0300

    added a button wich opens a way

---
+++ PlatformerGame_1/Assets/Scripts/ButtonRed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonRed : MonoBehaviour
+{
+    public GameObject[] blocks;
+    public Sprite turnOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Weight"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = turnOn;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+
+            foreach(GameObject block in blocks)
+            {
+                Destroy(block);
+            }
+        }
+    }
+}
