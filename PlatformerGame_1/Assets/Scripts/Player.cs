89cacb17e4e0aa15391d9bd240a9c3c00959fc75
ivan-zhukouski <stigmatix.by@gmail.com>
2020-12-14 15:12:02 +0300

    added a ladder with animation

--- PlatformerGame_1/Assets/Scripts/Player.cs
+++ PlatformerGame_1/Assets/Scripts/Player.cs
@@ -8,7 +8,8 @@ public class Player: MonoBehaviour
     public float moveSpeed;
     float jumpForce = 14f;
     float horizontalMove;
-    bool isGrounded;
+    float verticalMove;
+    bool isGrounded = true;
     public Transform groundCheck;
     Animator animator;
     int maxHP = 3;
@@ -20,6 +21,7 @@ public class Player: MonoBehaviour
     bool gotKey = false;
     bool canTP = true;
     public bool isSwimming = false;
+    bool isClimbing = false;
 
     void Start()
     {
@@ -33,11 +35,14 @@ public class Player: MonoBehaviour
     
     void Update()
     {
-        horizontalMove = Input.GetAxis("Horizontal");
-
         
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
 
-        if (isSwimming)
+
+
+
+        if (isSwimming && !isClimbing)
         {
             isGrounded = true;
             animator.SetInteger("State", 3);
@@ -47,13 +52,13 @@ public class Player: MonoBehaviour
         else
         {
             CheckGround();
-            if (horizontalMove == 0 && isGrounded)
+            if (horizontalMove == 0 && isGrounded && !isClimbing)
             {
                 animator.SetInteger("State", 0);
             }
             else
             {
-                if (isGrounded)
+                if (isGrounded && !isClimbing)
                 {
                     animator.SetInteger("State", 1);
                 }
@@ -63,7 +68,7 @@ public class Player: MonoBehaviour
                 FlipPlayer();
             }
         }
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isClimbing)
         {
             Jump();
         }
@@ -73,6 +78,10 @@ public class Player: MonoBehaviour
     {
         playerRB.velocity = new Vector2(horizontalMove * moveSpeed, playerRB.velocity.y);
         
+    }
+    void Climbing()
+    {
+        playerRB.velocity = new Vector2(playerRB.velocity.x, verticalMove * moveSpeed * 0.5f);
     }
 
     void FlipPlayer()
@@ -98,7 +107,7 @@ public class Player: MonoBehaviour
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
         
         isGrounded = colliders.Length > 1;
-        if(!isGrounded)
+        if(!isGrounded && !isClimbing)
         {
             animator.SetInteger("State", 2);
         }
@@ -154,5 +163,32 @@ public class Player: MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         canTP = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Ladder") )
+        {
+            isClimbing = true;
+            playerRB.gravityScale = 0f;
+            if (Input.GetAxis("Vertical") == 0)
+            {
+                animator.SetInteger("State", 4);  
+                playerRB.velocity = new Vector2(playerRB.velocity.x, verticalMove);
+            }
+            else
+            {
+                animator.SetInteger("State", 5);
+                Climbing();
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            isClimbing = false;
+            playerRB.gravityScale = 1f;
+        }
     }
 }
