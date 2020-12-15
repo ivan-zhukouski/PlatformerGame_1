131a47d8d28123b08dc2f8e0480a2ee79edd1b5c
ivan-zhukouski <stigmatix.by@gmail.com>
2020-12-15 23:07:11 +0300

    added BG with parallax effect

---
+++ PlatformerGame_1/Assets/Scripts/BG.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG : MonoBehaviour
+{
+    public GameObject cam;
+    float bgLength, startpos;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        bgLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float temp = cam.transform.position.x * (1 - parallaxEffect);
+
+        float distance = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+
+        if(temp > startpos + bgLength)
+        {
+            startpos += bgLength;
+        }else if(temp < startpos - bgLength)
+        {
+            startpos -= bgLength;
+        }
+    }
+}
