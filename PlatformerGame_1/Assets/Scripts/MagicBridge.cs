49788f25ea17b24920bdac1eef2be474e4057577
ivan-zhukouski <stigmatix.by@gmail.com>
2020-12-20 14:55:14 +0300

    added a quagmire

---
+++ PlatformerGame_1/Assets/Scripts/MagicBridge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBridge : MonoBehaviour
+{
+    public Transform rayLeft, rayRight;
+    private void OnTriggerStay2D(Collider2D collision)
+  
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            RaycastHit2D leftWall = Physics2D.Raycast(rayLeft.position, Vector2.left, 0.5f);
+            RaycastHit2D rightWall = Physics2D.Raycast(rayRight.position, Vector2.right, 0.5f);
+            if ( ((Input.GetAxis("Horizontal") < 0) && !leftWall.collider && (collision.transform.position.x < transform.position.x)) ||
+                 ((Input.GetAxis("Horizontal") > 0) && !rightWall.collider && (collision.transform.position.x > transform.position.x)))
+            {
+                transform.position = new Vector3(collision.transform.position.x, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
---
+++ PlatformerGame_1/Assets/Scripts/Quagmire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quagmire : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().mass *= 100f;
+            collision.gameObject.GetComponent<Player>().moveSpeed *= 0.25f;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().mass *= 0.01f;
+            collision.gameObject.GetComponent<Player>().moveSpeed *= 4;
+        }
+    }
+}
